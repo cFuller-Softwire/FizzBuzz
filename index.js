dc1fe3bc3ac9cdd89039647b50b578d355ff7248
Caspar Fuller <Caspar.Fuller@softwire.com>
2018-07-10 14:13:20 +0100

    Part 2a

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-for (let x = 1; x < 101; x++) {
-    if (x % 3 === 0 && x % 5 !== 0) {
-        console.log("Fizz");
-    } else if (x % 5 === 0 && x % 3 !== 0) {
-        console.log("Buzz");
-    } else if (x % 3 === 0 && x % 5 === 0) {
-        console.log("FizzBuzz");
-    } else {
-        console.log(x);
+function main() {
+    for (let x = 1; x < 101; x++) {
+        let multiString = "";
+        if(isThree(x)){
+            multiString = multiString.concat("Fizz");
+        }
+        if(isFive(x)) {
+            multiString = multiString.concat("Buzz");
+        }
+        if(isSeven(x)) {
+            multiString = multiString.concat("Bang");
+        }
+        if(multiString === "") {
+            console.log(x);
+        } else {
+            console.log(multiString);
+        }
+
     }
 }
+
+function isThree(x) {
+    if (x % 3 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isFive(x) {
+    if (x % 5 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isSeven(x) {
+    if (x % 7 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+main();
